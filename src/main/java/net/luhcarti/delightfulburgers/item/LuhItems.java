5068d73f2916c5512771458699d43553542c0d1b
luhcartimods <luhcartimods@gmail.com>
2024-05-05 12:21:33 +0200

    Fixed Chicken Burger Recipe, And Added Gradle Wrapper

--- src/main/java/net/luhcarti/delightfulburgers/item/LuhItems.java
+++ src/main/java/net/luhcarti/delightfulburgers/item/LuhItems.java
@@ -1,7 +1,9 @@
 package net.luhcarti.delightfulburgers.item;
 
+import com.mojang.blaze3d.shaders.Effect;
 import net.luhcarti.delightfulburgers.DelightfulBurgers;
 import net.luhcarti.delightfulburgers.food.LuhFoods;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,7 +15,7 @@ public class LuhItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, DelightfulBurgers.MOD_ID);
     public static final RegistryObject<Item> BACON_BURGER = ITEMS.register("bacon_burger",
             () -> new Item(new Item.Properties().food(LuhFoods.BACON_BURGER)));
-    public static final RegistryObject<Item> CHICKEN_BURGER = ITEMS.register("chicken_burger.json",
+    public static final RegistryObject<Item> CHICKEN_BURGER = ITEMS.register("chicken_burger",
             () -> new Item(new Item.Properties().food(LuhFoods.BACON_BURGER)));
 
     public static void register(IEventBus eventBus) {
